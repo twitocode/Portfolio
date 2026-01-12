09d024c42e9bbc091febe34b0d1c0c7040ed5147
twitocode <toheebeji@gmail.com>
2026-01-12 18:44:31 -0500

    twitodev -> toheebeji

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,7 +19,7 @@
 		href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Playwrite+IT+Moderna:wght@100..400&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
 		rel="stylesheet"
 	/>
-	<title>{data.url == '/' ? '' : data.url.split('/')[1] + ' |'} twitocode</title>
+	<title>{data.url == '/' ? '' : data.url.split('/')[1] + ' |'} toheebeji</title>
     <link rel="icon" type="image/x-icon" href="/logo.jpeg">
 </svelte:head>
 
