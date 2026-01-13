2549343b7992481278588877f51c03540106acfd
twitocode <toheebeji@gmail.com>
2026-01-13 08:31:26 -0500

    added vercel analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,7 @@
+import { dev } from "$app/environment";
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
+
+injectAnalytics({ mode: dev ? 'development' : 'production' });
 
 export function load({ url }) {
 	return {
